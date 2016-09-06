834ceaa61e2a6c572d7225c591871a2279cd7426
Nikita Seleckis <nikita@seleckis.lv>
2016-09-06 16:22:52 +0300

    move webpack.config.js to root dir

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var autoprefixer = require('autoprefixer');
 module.exports = {
 	devtool: 'cheap-module-eval-source-map',
 	entry: [
-		'./src/index.jsx'
+		'./example/src/index.jsx'
 	],
 	module: {
 		loaders: [
@@ -15,16 +15,12 @@ module.exports = {
 			}
 		]
 	},
-	externals: [
-		"react": "React",
-        "react-dom": "ReactDOM"
-	],
 	resolve: {
 		extensions: ['', '.js', '.jsx'],
 		modulesDirectories: ['node_modules']
 	},
 	output: {
-		path: __dirname + '/build/',
+		path: __dirname + '/example/build/',
 		publicPath: '/',
 		filename: 'index.js'
 	},
