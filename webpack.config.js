c59502a66b01f063d43842f651b07b53ad43347f
Nikita Seleckis <nikita@seleckis.lv>
2017-05-25 14:26:05 +0300

    changed babel settings, moved to .babelrc, updated all packages, removed unused

--- webpack.config.js
+++ webpack.config.js
@@ -3,37 +3,34 @@ var autoprefixer = require('autoprefixer');
 
 module.exports = {
 	devtool: 'cheap-module-eval-source-map',
-	entry: [
-		'./example/src/index.jsx'
-	],
+	entry: ['./example/src/index.jsx'],
 	module: {
-		loaders: [
+		rules: [
 			{
-				test: /\.jsx?$/,
-				exclude: /node_modules/,
-				loader: 'eslint!babel'
-			}
+				test: /\.css$/,
+				use: [
+					'style-loader', {
+						loader: 'css-loader',
+						options: {
+							importLoaders: 1
+						}
+					},
+					'postcss-loader'
+				]
+			},
+			{
+        		test: /\.jsx?$/,
+				exclude: [/node_modules/],
+				use: [
+					'eslint-loader',
+					'babel-loader',
+				]
+			},
 		]
 	},
-	resolve: {
-		extensions: ['', '.js', '.jsx'],
-		modulesDirectories: ['node_modules']
-	},
-	output: {
+	output : {
 		path: __dirname + '/example/build/',
 		publicPath: '/',
 		filename: 'index.js'
 	},
-	plugins: [
-		new webpack.optimize.OccurenceOrderPlugin(),
-		new webpack.NoErrorsPlugin()
-	],
-	postcss: function () {
-        return {
-            defaults: [autoprefixer],
-            cleaner:  [autoprefixer({ browsers: [
-				'> 1%', 'IE >= 10', 'last 2 versions', 'Android >= 5'
-			] })]
-        };
-    }
 };
