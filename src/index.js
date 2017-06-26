3e5fa9c0e9b7b74f2230d3e13779f8e5d48d7f77
Sean Neden <sean@sagansystems.com>
2017-06-26 09:04:09 -0700

    fix pause on unmount

--- src/index.js
+++ src/index.js
@@ -1,75 +1,73 @@
 /*global Stickyfill */
-import React, {Component} from "react";
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
 import 'Stickyfill';
 
 export default class Sticker extends Component {
-	static propTypes = {
-	    media: PropTypes.string,
-		children: PropTypes.oneOfType([
-			PropTypes.element,
-			PropTypes.func
-		])
-	};
-	constructor(props) {
-		super(props);
-		this.state = {
-			isSticky: false
-		};
-	}
-	mediaMatch = (media) => {
-		return window && window.matchMedia(media).matches;
-	}
-	sticky = (stick) => {
-		Stickyfill.add(stick);
-		this.setState({
-			isSticky: true
-		});
-	}
-	unsticky = (stick) => {
-		Stickyfill.remove(stick);
-		this.setState({
-			isSticky: false
-		});
-	}
-	update = () => {
-		Stickyfill.rebuild();
-	}
-	handleResize = () => {
-		if(this.mediaMatch(this.props.media)){
-			if(!this.state.isSticky){
-				this.sticky(this.stick);
-			}
-		} else if(this.state.isSticky){
-			this.unsticky(this.stick);
-		}
-	}
-	componentDidMount(){
-		this.stick = ReactDOM.findDOMNode(this);
-		if(this.props.media){
-			window && window.addEventListener('resize', this.handleResize);
-			this.handleResize();
-		} else {
-			this.sticky(this.stick);
-		}
-	}
-	componentWillUnmount() {
-		if(this.props.media){
-			window && window.removeEventListener('resize', this.handleResize);
-		}
-		this.unsticky(this.stick);
-	}
-	componentWillReceiveProps(nextProps){
-		if(nextProps.forceUpdate !== this.props.forceUpdate){
-			this.update();
-		}
-	}
-	componentDidUpdate(){
-		this.update();
-	}
-	render() {
-		let { children, ...otherProps } = this.props;
-		return typeof children.type === "function" ? React.cloneElement(this.props.children, { ...otherProps }) : children;
-	}
+  static propTypes = {
+    media: PropTypes.string,
+    children: PropTypes.oneOfType([PropTypes.element, PropTypes.func]),
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSticky: false,
+    };
+  }
+  mediaMatch = media => {
+    return window && window.matchMedia(media).matches;
+  };
+  sticky = stick => {
+    Stickyfill.add(stick);
+    this.setState({
+      isSticky: true,
+    });
+  };
+  unsticky = stick => {
+    Stickyfill.remove(stick);
+    this.setState({
+      isSticky: false,
+    });
+  };
+  update = () => {
+    Stickyfill.rebuild();
+  };
+  handleResize = () => {
+    if (this.mediaMatch(this.props.media)) {
+      if (!this.state.isSticky) {
+        this.sticky(this.stick);
+      }
+    } else if (this.state.isSticky) {
+      this.unsticky(this.stick);
+    }
+  };
+  componentDidMount() {
+    this.stick = ReactDOM.findDOMNode(this);
+    if (this.props.media) {
+      window && window.addEventListener('resize', this.handleResize);
+      this.handleResize();
+    } else {
+      this.sticky(this.stick);
+    }
+  }
+  componentWillUnmount() {
+    if (this.props.media) {
+      window && window.removeEventListener('resize', this.handleResize);
+    }
+    this.unsticky(this.stick);
+    Stickyfill.pause();
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.forceUpdate !== this.props.forceUpdate) {
+      this.update();
+    }
+  }
+  componentDidUpdate() {
+    this.update();
+  }
+  render() {
+    let { children, ...otherProps } = this.props;
+    return typeof children.type === 'function' ? React.cloneElement(this.props.children, { ...otherProps }) : children;
+  }
 }
